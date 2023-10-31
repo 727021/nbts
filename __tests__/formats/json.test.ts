9df46d4802261f16c4996f3f9ad096bae4220428
Andrew Schimelpfening <1834544+727021@users.noreply.github.com>
2023-10-31 22:59:25 +0000

    wribaseline tests for json format

--- __tests__/formats/json.test.ts
+++ __tests__/formats/json.test.ts
@@ -1,5 +1,39 @@
+import { deserialize, serialize } from '../../src/formats/json'
+import { bigTest, helloWorld, playerNanValue } from '../data/test-data'
+
 describe('json serializer', () => {
-    it('passes', () => {
-        expect(true).toBeTruthy()
+    it('serializes hello world correctly', () => {
+        const result = serialize(helloWorld)
+        expect(result).toMatchSnapshot()
+    })
+
+    it('serializes bigtest correctly', () => {
+        const result = serialize(bigTest)
+        expect(result).toMatchSnapshot()
+    })
+
+    it('serializes player-nan-value correvtly', () => {
+        const result = serialize(playerNanValue)
+        expect(result).toMatchSnapshot()
+    })
+})
+
+describe('json deserializer', () => {
+    it('deserializes hello world correctly', () => {
+        const json = serialize(helloWorld)
+        const result = deserialize(Buffer.from(json))
+        expect(result).toEqual(helloWorld)
+    })
+
+    it('deserializes bigtest correctly', () => {
+        const json = serialize(bigTest)
+        const result = deserialize(Buffer.from(json))
+        expect(result).toEqual(bigTest)
+    })
+
+    it('deserializes player-nan-value correctly', () => {
+        const json = serialize(playerNanValue)
+        const result = deserialize(Buffer.from(json))
+        expect(result).toEqual(playerNanValue)
     })
 })
